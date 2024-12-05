eef060c176a08942e3b96f0598a1305a06ffa821
susmita <susmitaghosh.leelija@gmail.com>
2024-12-05 15:58:17 +0530

    headers change

--- api/headers.php
+++ api/headers.php
@@ -23,10 +23,10 @@ if ($server === '/superadmin-medicy/') {
 }
 
 // Set headers for CORS and response content type
-// if (in_array($origin, $allowed_origins)) {
+if (in_array($origin, $allowed_origins)) {
     // echo $origin;
     header("Access-Control-Allow-Origin: $origin");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     header("Content-Type: application/json; charset=UTF-8");
-// }
+}
